fda605c1a73e7fafee09ef9cd3ab1135a9d055f3
Abhishek Ashok Borde <abhishekborde2000@gmail.com>
2022-06-24 15:13:38 +0530

    Error in loading the pickle file
    Pickle file written in binary so it should also read in the binary

--- server/util.py
+++ server/util.py
@@ -1,9 +1,11 @@
+from glob import glob
 import json
 import pickle
 
 __locations = None      # Global varibles declared double underscore makes it private attribute
 __data_columns = None
-__model = None
+__model1 = None
+
 
 def get_location_names():
     return __locations
@@ -12,13 +14,14 @@ def load_saved_artifacts():
     print("loading saved artifacts ...start")
     global __data_columns
     global __locations
+    global __model1
     
     with open("./artifacts/columns.json", "r") as f: 
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
     
-    with open("./artifacts/banglore_home_prices_model.pickle", "r") as f: 
-        __model = pickle.load(f)
+    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f: 
+        __model1 = pickle.load(f)
     print("Loading artifacts ...done.") 
     
 if __name__ == '__main__':
