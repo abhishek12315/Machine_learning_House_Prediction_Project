70fcee14b19c7a5c3ed9fe317202fb4c05cf8f52
Abhishek Ashok Borde <abhishekborde2000@gmail.com>
2022-06-24 19:40:10 +0530

    JS, CSS and HTML files are pending
    Currently we need to figure way out to model again using Flask backend again

--- server/util.py
+++ server/util.py
@@ -15,11 +15,11 @@ def load_saved_artifacts():
     global __locations
     global __model1
     
-    with open("./artifacts/columns.json", "r") as f: 
+    with open("./server/artifacts/columns.json", "r") as f: 
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
     
-    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f: 
+    with open("./server/artifacts/banglore_home_prices_model.pickle", "rb") as f: 
         __model1 = pickle.load(f)
     print("Loading artifacts ...done.") 
 
