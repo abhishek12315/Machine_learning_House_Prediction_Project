4ef7d5fd7d754bd66fb7c868dcf34d120f677453
Abhishek Ashok Borde <abhishekborde2000@gmail.com>
2022-06-24 16:20:46 +0530

    After the postman GET and POST requests checked

--- server/util.py
+++ server/util.py
@@ -1,11 +1,10 @@
-from glob import glob
 import json
 import pickle
+import numpy as np
 
 __locations = None      # Global varibles declared double underscore makes it private attribute
 __data_columns = None
 __model1 = None
-
 
 def get_location_names():
     return __locations
@@ -23,8 +22,31 @@ def load_saved_artifacts():
     with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f: 
         __model1 = pickle.load(f)
     print("Loading artifacts ...done.") 
+
+def get_estimated_price(location, sqft, bhk, bath): 
+    try: 
+        loc_index = __data_columns.index(location.lower()) 
+    except: 
+        loc_index = -1
     
+    m = np.zeros(len(__data_columns))
+    m[0] = sqft
+    m[1] = bath
+    m[2] = bhk
+    
+    if loc_index >= 0: 
+        m[loc_index] = 1
+    
+    return round(__model1.predict([m])[0],2)
+
+
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('Kalhalli', 1000, 3, 3)) # Other Location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # Other Location
+    
+    
     
